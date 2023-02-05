2e4c2cfcad94aaeaea1a33d7d443e5b6ffcb6bcf
mvill <119754846+migavg@users.noreply.github.com>
2023-02-04 23:22:58 -0700

    set up api in js to log current temp in phoenix on browser console

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,14 @@ var searchFormElement = document.querySelector("#search-form");
 var searchButton = document.getElementById("search-btn");
 
 function getApi() {
-    var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=1585e0bbf2dbcb702a5708a67bf3b74d";
+    var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=1585e0bbf2dbcb702a5708a67bf3b74d&units=imperial";
 
     fetch(weatherUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
+            console.log(data.main.temp);
 
         })
 
