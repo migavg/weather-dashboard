42807d23acd84d2c100150385ec683640baa1f1b
mvill <119754846+migavg@users.noreply.github.com>
2023-02-05 21:38:26 -0700

    added humidity and wind speed along with a simple concat to show in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,36 @@ var weatherCity =  document.getElementById("citySearch");
 function getApi() {
 
     var citySearch = weatherCity.value;
-    var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=1585e0bbf2dbcb702a5708a67bf3b74d&units=imperial";
+    var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=1585e0bbf2dbcb702a5708a67bf3b74d&units=imperial&weather";
 
     fetch(weatherUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+                console.log(data);
                 console.log(data.main.temp);
+                console.log(data.wind.speed);
+                console.log(data.main.humidity);
                 console.log(citySearch);
+
         var currentCity = document.createElement("p");
         var currentTemp = document.createElement("p");
-        currentTemp.textContent = data.main.temp;
-        currentCity.textContent = citySearch;
+        var currentWind = document.createElement("p");
+        var currentHum = document.createElement("p");
+
+        currentCity.textContent = "Current Temp in: " + citySearch;
+        currentTemp.textContent = "Temperature " + data.main.temp + "°F";
+        currentWind.textContent = "Wind Speed: " + data.wind.speed + "MPH";
+        currentHum.textContent = "Humidity " + data.main.humidity + "%";
+
+        
 
         weatherInfo.append(currentCity);
         weatherInfo.append(currentTemp);
+        weatherInfo.append(currentWind);
+        weatherInfo.append(currentHum);
+
             
         })
 
