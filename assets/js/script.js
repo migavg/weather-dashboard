037fd4b603cae191c29b8a3af78d178ee1666676
mvill <119754846+migavg@users.noreply.github.com>
2023-12-26 14:55:47 -0700

    made search button clear previously searched cities and info by clearing out the inner html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var searchButton = document.getElementById("search-btn");
 var weatherInfo = document.getElementById("weather-info-container");
 var weatherCity = document.getElementById("citySearch");
-var weatherForecast =document.getElementById("weather-forecast") ;
+var weatherForecast = document.getElementById("weather-forecast");
 var weatherForecast1 = document.getElementById("forecast-one");
 var weatherForecast2 = document.getElementById("forecast-two");
 var weatherForecast3 = document.getElementById("forecast-three");
@@ -9,7 +9,9 @@ var weatherForecast4 = document.getElementById("forecast-four");
 var weatherForecast5 = document.getElementById("forecast-five");
 
 
-    // Function that grabs current weather and appends it to a div, then runs 5 day forecast
+// Function that grabs current weather and appends it to a div, then runs 5 day forecast
+
+
 
 function getWeather() {
 
@@ -32,27 +34,34 @@ function getWeather() {
             var currentWind = document.createElement("p");
             var currentHum = document.createElement("p");
 
+
+
+            /// clears the previous search
+            weatherInfo.innerHTML = "";
+
             currentCity.textContent = "Current Temp in: " + citySearch;
             currentTemp.textContent = "Temperature " + data.main.temp + "°F";
             currentWind.textContent = "Wind Speed: " + data.wind.speed + "MPH";
             currentHum.textContent = "Humidity " + data.main.humidity + "%";
-            
+
+
+
             weatherInfo.append(currentCity);
             weatherInfo.append(currentTemp);
             weatherInfo.append(currentWind);
             weatherInfo.append(currentHum);
         })
-    
-        weatherInfo.classList.add("weatherCard");
+
+    weatherInfo.classList.add("weatherCard");
 
 
     fiveDayForecast();
 };
 
 
-    // Function that grabs 5 day forecast and appends it to respective div in html  
-    // (had a for loop to make it shorter but in the end I couldn't append it to each independent div ID) 
-    // Kept my console.log data in order to see if thr data was populating correctly and also used as reference point while looking in the api array 
+// Function that grabs 5 day forecast and appends it to respective div in html  
+// (had a for loop to make it shorter but in the end I couldn't append it to each independent div ID) 
+// Kept my console.log data in order to see if thr data was populating correctly and also used as reference point while looking in the api array 
 
 function fiveDayForecast() {
     var citySearch = weatherCity.value;
@@ -64,7 +73,7 @@ function fiveDayForecast() {
         })
         .then(function (data) {
 
-    // Day one of forecast 
+            // Day one of forecast 
             console.log(data.list[0].main.temp);
             console.log(data.list[0].wind.speed);
             console.log(data.list[0].main.humidity);
@@ -73,6 +82,9 @@ function fiveDayForecast() {
             var wind = document.createElement("p");
             var humidity = document.createElement("p");
             var day = document.createElement("p");
+
+            //clears previous card
+            weatherForecast1.innerHTML = "";
 
             day.textContent = "Day 1"
             temp.textContent = "Temperature " + data.list[0].main.temp + "°F";
@@ -84,7 +96,7 @@ function fiveDayForecast() {
             weatherForecast1.appendChild(wind);
             weatherForecast1.appendChild(humidity);
 
-    // Day two of forecast 
+            // Day two of forecast 
             console.log(data.list[1].main.temp);
             console.log(data.list[1].wind.speed);
             console.log(data.list[1].main.humidity);
@@ -93,6 +105,11 @@ function fiveDayForecast() {
             var wind = document.createElement("p");
             var humidity = document.createElement("p");
             var day = document.createElement("p");
+
+
+            //clears previous card
+            weatherForecast2.innerHTML = "";
+
 
             day.textContent = "Day 2"
             temp.textContent = "Temperature " + data.list[1].main.temp + "°F";
@@ -103,8 +120,8 @@ function fiveDayForecast() {
             weatherForecast2.appendChild(temp);
             weatherForecast2.appendChild(wind);
             weatherForecast2.appendChild(humidity);
-           
-    // Day three of forecast 
+
+            // Day three of forecast 
             console.log(data.list[2].main.temp);
             console.log(data.list[2].wind.speed);
             console.log(data.list[2].main.humidity);
@@ -113,6 +130,10 @@ function fiveDayForecast() {
             var wind = document.createElement("p");
             var humidity = document.createElement("p");
             var day = document.createElement("p");
+
+
+            //clears previous card
+            weatherForecast3.innerHTML = "";
 
             day.textContent = "Day 3"
             temp.textContent = "Temperature " + data.list[2].main.temp + "°F";
@@ -124,7 +145,7 @@ function fiveDayForecast() {
             weatherForecast3.appendChild(wind);
             weatherForecast3.appendChild(humidity);
 
-    // Day four of forecast 
+            // Day four of forecast 
             console.log(data.list[3].main.temp);
             console.log(data.list[3].wind.speed);
             console.log(data.list[3].main.humidity);
@@ -133,6 +154,9 @@ function fiveDayForecast() {
             var wind = document.createElement("p");
             var humidity = document.createElement("p");
             var day = document.createElement("p");
+
+            //clears previous card
+            weatherForecast4.innerHTML = "";
 
             day.textContent = "Day 4"
             temp.textContent = "Temperature " + data.list[3].main.temp + "°F";
@@ -144,7 +168,7 @@ function fiveDayForecast() {
             weatherForecast4.appendChild(wind);
             weatherForecast4.appendChild(humidity);
 
-    // Day five of forecast 
+            // Day five of forecast 
             console.log(data.list[4].main.temp);
             console.log(data.list[4].wind.speed);
             console.log(data.list[4].main.humidity);
@@ -153,6 +177,10 @@ function fiveDayForecast() {
             var wind = document.createElement("p");
             var humidity = document.createElement("p");
             var day = document.createElement("p");
+
+            //clears previous card
+            weatherForecast5.innerHTML = "";
+
 
             day.textContent = "Day 5"
             temp.textContent = "Temperature " + data.list[4].main.temp + "°F";
@@ -164,16 +192,19 @@ function fiveDayForecast() {
             weatherForecast5.appendChild(wind);
             weatherForecast5.appendChild(humidity);
         })
-        
+
     // Adds classes to the weather forecast divs for basic styling git 
-        
-        weatherForecast.classList.add("weatherCardContainer");
-        weatherForecast1.classList.add("weatherCard");
-        weatherForecast2.classList.add("weatherCard");
-        weatherForecast3.classList.add("weatherCard");
-        weatherForecast4.classList.add("weatherCard");
-        weatherForecast5.classList.add("weatherCard");
-        
+
+    weatherForecast.classList.add("weatherCardContainer");
+    weatherForecast1.classList.add("weatherCard");
+    weatherForecast2.classList.add("weatherCard");
+    weatherForecast3.classList.add("weatherCard");
+    weatherForecast4.classList.add("weatherCard");
+    weatherForecast5.classList.add("weatherCard");
+
+
+
+
 };
 
 
